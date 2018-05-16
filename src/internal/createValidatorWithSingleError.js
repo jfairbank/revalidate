c48e51d2c43b3b5230aff7ea57a6107c89c84c0d
Edward D'Souza <edward.dsouza@polarmobile.com>
2018-05-16 16:08:27 -0400

    Add props as extra argument to validators.

--- src/internal/createValidatorWithSingleError.js
+++ src/internal/createValidatorWithSingleError.js
@@ -5,15 +5,15 @@ export default function createValidatorWithSingleError(
   validators: Array<Validator>,
   sharedConfig: ComposeConfig,
 ): ConfiguredValidator {
-  return function composedValidator(value, allValues) {
+  return function composedValidator(value, allValues, props) {
     for (let i = 0, l = validators.length; i < l; i++) {
       const validator = validators[i];
       let errorMessage;
 
       if (isValueValidator(validator)) {
-        errorMessage = validator(value, allValues);
+        errorMessage = validator(value, allValues, props);
       } else {
-        errorMessage = validator(sharedConfig, value, allValues);
+        errorMessage = validator(sharedConfig, value, allValues, props);
       }
 
       if (errorMessage) {
